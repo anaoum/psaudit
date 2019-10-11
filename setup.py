71851c2d116e78b32ac4801f101799ca4974cf10
Andrew Naoum <andrew@naoum.com>
2019-10-11 22:10:34 +1100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='psaudit',
-    version='0.1.0',
+    version='0.1.1',
     description='Process auditor for macOS',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Andrew Naoum',
     author_email='andy.naoum@gmail.com',
     url='https://github.com/anaoum/psaudit',
     scripts=['psaudit'],
     install_requires=['psutil>=5.6.3'],
+    license='MIT',
+    download_url='https://github.com/anaoum/psaudit/archive/0.1.1.tar.gz',
+    classifiers=[
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
 )
