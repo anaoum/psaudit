73e83fe0b8cfe42ee8f8e620de95720be17eb28b
Andrew Naoum <andrew@naoum.com>
2019-10-11 19:47:15 +1100

    use threading

--- audit.py
+++ audit.py
@@ -3,12 +3,29 @@
 import os
 import re
 import sys
-import asyncio
+import threading
+import queue
 import subprocess
 
 import psutil
 
-MAX_PROCS = 100
+MAX_THREADS = 16
+
+failed = 0
+
+process_paths = {}
+for p in psutil.process_iter(attrs={'pid', 'exe'}):
+    pid, path = p.info['pid'], p.info['exe']
+    if pid == 0:
+        continue
+    elif pid == os.getpid():
+        continue
+    elif path is None:
+        print('could not determine executable for process', pid, file=sys.stderr)
+        failed += 1
+        continue
+    process_paths[pid] = path
+paths = set(process_paths.values())
 
 whitelist = [
         re.compile('^Identifier=com\.apple\..*'),
@@ -20,49 +37,48 @@ whitelist = [
         re.compile('^Authority=.*UBF8T346G9.*'), # Microsoft
 ]
 
-process_paths = {}
-for p in psutil.process_iter(attrs={'pid', 'exe'}):
-    if p.info['pid'] == 0:
-        continue
-    elif p.info['pid'] == os.getpid():
-        continue
-    process_paths[p.info['pid']] = p.info['exe']
-
-process_audits = {}
-
-async def audit_process(pid):
-    path = process_paths[pid]
+def audit_process(path):
     if path is None:
-        process_audits[pid] = None
-        return
-    p = await asyncio.create_subprocess_shell(f'codesign -dvv "{path}"', stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
-    stdout, stderr = await p.communicate()
-    for line in stderr.decode('utf-8').split('\n'):
+        return None
+    p = subprocess.run(['codesign', '-dvv', path], capture_output=True)
+    for line in p.stderr.decode('utf-8').split('\n'):
         for expr in whitelist:
             if expr.match(line):
-                process_audits[pid] = True
-                return
-    process_audits[pid] = False
+                return True
+    return False
 
-async def audit():
-    remaining = list(process_paths.keys())
-    while remaining:
-        tasks = []
-        while len(tasks) < MAX_PROCS and remaining:
-            tasks.append(asyncio.create_task(audit_process(remaining.pop(0))))
-        for task in tasks:
-            await task
+def auditor(audit_queue, audit_results_queue):
+    while True:
+        path = audit_queue.get()
+        if path is None:
+            break
+        result = audit_process(path)
+        audit_results_queue.put((path, result))
 
-asyncio.run(audit())
+audit_queue = queue.Queue()
+audit_results_queue = queue.Queue()
 
-failed = 0
+for path in paths:
+    audit_queue.put(path)
 
-for pid, audit_result in process_audits.items():
-    if audit_result is None:
-        print('could not determine executable for process', pid, file=sys.stderr)
-        failed += 1
-    elif audit_result is False:
+threads = []
+for _ in range(MAX_THREADS):
+    t = threading.Thread(target=auditor, args=(audit_queue, audit_results_queue))
+    t.start()
+    threads.append(t)
+
+path_audits = {}
+while len(path_audits) < len(paths):
+    path, result = audit_results_queue.get()
+    path_audits[path] = result
+for _ in range(MAX_THREADS):
+    audit_queue.put(None)
+for t in threads:
+    t.join()
+
+for pid, path in process_paths.items():
+    audit_result = path_audits[path]
+    if audit_result is False:
         print(f'process {pid} ({process_paths[pid]}) not in whitelist', file=sys.stderr)
         failed += 1
-
 sys.exit(failed)
