6275f469ab5d87e9d231926834ae3161221a4b18
Andrew Naoum <andrew@naoum.com>
2019-10-11 18:42:46 +1100

    multiproc

--- audit.py
+++ audit.py
@@ -3,9 +3,12 @@
 import os
 import re
 import sys
+import asyncio
 import subprocess
 
 import psutil
+
+MAX_PROCS = 100
 
 whitelist = [
         re.compile('^Identifier=com\.apple\..*'),
@@ -17,26 +20,49 @@ whitelist = [
         re.compile('^Authority=.*UBF8T346G9.*'), # Microsoft
 ]
 
-def in_whitelist(process_path):
-    p = subprocess.run(['codesign', '-dvv', process_path], capture_output=True)
-    for line in p.stderr.decode('utf-8').split('\n'):
-        for expr in whitelist:
-            if expr.match(line):
-                return True
-    return False
-
-failed = 0
-
+process_paths = {}
 for p in psutil.process_iter(attrs={'pid', 'exe'}):
     if p.info['pid'] == 0:
         continue
     elif p.info['pid'] == os.getpid():
         continue
-    elif p.info['exe'] is None:
-        print('could not determine executable for process', p.info['pid'], file=sys.stderr)
+    process_paths[p.info['pid']] = p.info['exe']
+
+process_audits = {}
+
+async def audit_process(pid):
+    path = process_paths[pid]
+    if path is None:
+        process_audits[pid] = None
+        return
+    p = await asyncio.create_subprocess_shell(f'codesign -dvv "{path}"', stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+    stdout, stderr = await p.communicate()
+    for line in stderr.decode('utf-8').split('\n'):
+        for expr in whitelist:
+            if expr.match(line):
+                process_audits[pid] = True
+                return
+    process_audits[pid] = False
+
+async def audit():
+    remaining = list(process_paths.keys())
+    while remaining:
+        tasks = []
+        while len(tasks) < MAX_PROCS and remaining:
+            tasks.append(asyncio.create_task(audit_process(remaining.pop(0))))
+        for task in tasks:
+            await task
+
+asyncio.run(audit())
+
+failed = 0
+
+for pid, audit_result in process_audits.items():
+    if audit_result is None:
+        print('could not determine executable for process', pid, file=sys.stderr)
         failed += 1
-    elif not in_whitelist(p.info['exe']):
-        print(f'process {p.info["pid"]} ({p.info["exe"]}) not in whitelist', file=sys.stderr)
+    elif audit_result is False:
+        print(f'process {pid} ({process_paths[pid]}) not in whitelist', file=sys.stderr)
         failed += 1
 
 sys.exit(failed)
